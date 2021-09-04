07dd215107643d294723a052e0165dfca31ed1b5
harveyhq <harveyspecterpro@protonmail.com>
2021-09-04 18:53:17 +0300

    refactoring

---
+++ src/index.js
@@ -0,0 +1,12 @@
+/**
+ * Test function
+ *
+ * @param {String} name
+ * @returns Uppercased string
+ */
+function test(name) {
+    return name === typeof String && name.toUpperCase();
+}
+
+// Run
+console.log(test("dEmO tExT"));
